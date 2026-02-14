4554b45c9585c408af197f15183839630e2443d2
Chris Leahy <leahycm@gmail.com>
2026-02-14 12:20:17 -0500

    Add 2pBb as a one leg connection since it can be used with Queen

--- src/card_checks_acros.rs
+++ src/card_checks_acros.rs
@@ -904,7 +904,8 @@ fn check_group_c_positions(acro: &TeamAcrobatic) -> CardIssues {
 
 fn check_positions(acro: &TeamAcrobatic) -> CardIssues {
     const ONE_LEG_CONNECTIONS: &[&str] = &[
-        "F1S", "1F1P", "1F1F", "FAb", "3pA", "1FA", "3pS", "3pbA", "FA+PF", ">F1P", "FP", "ShiShi+",
+        "F1S", "1F1P", "1F1F", "FAb", "3pA", "1FA", "3pS", "3pbA", "FA+PF", ">F1P", "FP",
+        "ShiShi+", "2pBb",
     ];
     let mut ci = CardIssues::default();
 
